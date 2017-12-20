b4739dffc95fe0eeea764400a0110ff6dec9dcab
h2562961224 <2562961224@qq.com>
2017-12-20 09:12:35 +0800

    edit

--- spring-cloud-client-demo2/src/main/java/com/huxiaohui/controller/HelloViewController.java
+++ spring-cloud-client-demo2/src/main/java/com/huxiaohui/controller/HelloViewController.java
@@ -1,5 +1,9 @@
 package com.huxiaohui.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +16,13 @@ public class HelloViewController {
     private HelloService helloService;
 
     @RequestMapping("/getHelloFeign")
-    public String from() {
-        return helloService.getGetHello();
+    public void from(HttpServletResponse response) {
+    	response.setContentType("text/html"); 
+    	try {
+			response.getWriter().print(helloService.getGetHello());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+        return ;
     }
 }
